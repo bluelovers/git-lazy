478c6f6cfb7454fe93b6506513b7eca9662a457d
bluelovers <codelovers@users.sourceforge.net>
2017-03-21 15:37:25 +0800

    . (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	.env
    	bin/notepad.js
    	src/bin/index.js
    	src/lazy/bin.js
    
    [ M] modified - Changes not staged for commit:
    
    	bin/git-rename.js
    	src/lazy/parse.js
    	src/lazy/path.js

---
+++ bin/notepad.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+/**
+ * Created by user on 2017/3/21.
+ */
+
+require('../src/bin');
+
+const cli = meow();
+
+let output = gitlazy.notepad(cli.input[0])
+.then((data) =>
+{
+	console.log(data.data);
+})
+.catch((err) =>
+	{
+		console.error(`${err}`);
+		process.exit(1);
+		//throw err;
+	}
+);
