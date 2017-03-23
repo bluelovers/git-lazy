e3980ddbcf92aa7661770edf6871c4f94362fa8b
bluelovers <codelovers@users.sourceforge.net>
2017-03-23 17:00:11 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	src/lazy/branch.js
    	src/util/cli/console.js
    
    [ M] modified - Changes not staged for commit:
    
    	src/async/child_process.js
    	src/bin/index.js
    	src/lazy/lib.js
    	src/lazy/path.js

---
+++ src/lazy/branch.js
@@ -0,0 +1,104 @@
+/**
+ * Created by user on 2017/3/22.
+ */
+
+'use strict';
+
+const LF = "\n";
+const fs = require('../async/fs');
+const path = require('upath2');
+
+const self = module.exports = Object.assign(require("."), {
+
+		async git_branch_new_empty(branch_name, options = {})
+		{
+			options = Object.assign({
+					_deep: 2,
+					/*
+					 * @BUG
+					 { error: null,
+					 stdout: '',
+					 stderr: 'Switched to a new branch \'123\'\n' }
+					 */
+					fallback_stderr: true,
+				}, options
+			);
+
+			if (!branch_name)
+			{
+				throw new TypeError(`fatal: '${branch_name}' is not a valid branch name.`);
+			}
+
+			const cwd = process.cwd();
+
+			return self.repo_dir_root()
+			.then((dir_root) =>
+				{
+					let p = path.relative(cwd, dir_root);
+
+					// safe check
+					if (p == '' || p == '.' || (options._deep > 1 && p.split('/').length < options._deep))
+					{
+						if (p == '')
+						{
+							p = '.';
+						}
+
+						return p;
+					}
+					else
+					{
+						throw new Error(`cwd must near root="${dir_root}"`)
+					}
+				}
+			)
+			.then((path_relative) =>
+				{
+
+					return self.execEach([
+							`git checkout --orphan "${branch_name}"`,
+							`git rm -r --cached "${path_relative}"`,
+						], options
+					)
+					.then((res) =>
+						{
+							//res.push(`Create a new empty orphan branch '${branch_name}'`);
+
+							return res.res.join(LF + LF);
+						}
+					);
+
+					/*
+					 let res = [];
+
+					 return self.exec(`git checkout --orphan "${branch_name}"`, {
+					 fallback_stderr: true,
+					 }
+					 )
+					 .then((stdout) =>
+					 {
+					 res.push(stdout);
+
+					 return self.exec(`git rm -r --cached "${path_relative}"`);
+					 }
+					 )
+					 .then((stdout) =>
+					 {
+					 res.push(stdout);
+
+					 return res.join(LF + LF);
+					 }
+					 );
+					 */
+
+				}
+			)
+			.catch((err) =>
+				{
+					throw err;
+				}
+			);
+		},
+
+	}
+);
