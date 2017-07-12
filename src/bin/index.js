6ecfb1a7720ed7ae821a2c63acfd8f4e2380bb03
bluelovers <codelovers@users.sourceforge.net>
2017-07-12 18:39:52 +0800

    . (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	bin/git-gc2
    	src/lazy/database.js
    
    [ M] modified - Changes not staged for commit:
    
    	src/bin/index.js
    	src/lazy/branch.js
    	src/util/cli/console.js

--- src/bin/index.js
+++ src/bin/index.js
@@ -7,13 +7,18 @@
 	const gitlazy = require('../lazy');
 	const meow = require('meow');
 
+	const time_label = 'Process finished';
+
 	require('dotenv').config();
 
 	_global_.gitlazy = gitlazy;
 	_global_.meow = meow;
+	_global_.time_label = time_label;
 
 	_global_.chalk = require('chalk');
 
 	_global_.lazyConsole = require('../util/cli/console').Console.create();
+
+	_global_.lazyConsole.time(time_label);
 
 })(global);
